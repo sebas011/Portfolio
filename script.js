9cbf72755c7c26b50090b393ab1c5c26aadbf97b
Rodian Marin <rodianmarin@gmail.com>
2024-02-11 17:15:51 +0800

    new update

--- script.js
+++ script.js
@@ -1,16 +1,9 @@
 function toggleMenu() {
-    var menuBtn = document.querySelector('.menu-btn');
-    var navLinks = document.getElementById('navLinks');
-    
-    // Toggle the active class on the menu button
-    menuBtn.classList.toggle('active');
-
-    // Check if the menu button is now active
-    if (menuBtn.classList.contains('active')) {
-        // If the menu button is active, display the navigation links
-        navLinks.style.display = 'block';
+    var menuOptions = document.getElementById("menuOptions");
+    if (menuOptions.style.display === "block") {
+      menuOptions.style.display = "none";
     } else {
-        // If the menu button is not active, hide the navigation links
-        navLinks.style.display = 'none';
+      menuOptions.style.display = "block";
     }
-}
+  }
+  
